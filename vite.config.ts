a9d6ae4524e928274667589243e9dd2550d72347
paulreitz <avenose@gmail.com>
2025-05-09 07:05:39 -0700

    Deploy github page.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+const getBase = () => {
+  if (process.env.NODE_ENV === "development") return "/";
+  return `/${
+    process.env.GITHUB_REPOSITORY?.split("/")[1] || "external_state_example"
+  }/`;
+};
 
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
+  base: getBase(),
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
